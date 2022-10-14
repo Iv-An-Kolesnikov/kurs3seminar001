1bc50a52986fd657c18cb618621071df244f68da
Iv-An-Kolesnikov <i_a_kolesnikov@list.ru>
2022-10-14 13:02:59 +0300

    task002 finich2

--- task002/Program.cs
+++ task002/Program.cs
@@ -4,8 +4,10 @@
 // 5 => Пятница
 Console.Clear();
 int num;
-Console.WriteLine("Введите число");
+Console.WriteLine("Введите целое число от 1 до 7");
 num = Convert.ToInt32(Console.ReadLine());
+if (num > 7) Console.WriteLine("Вы ввели не верное число, введите целое чисто от 1 до 7");
+if (num < 0) Console.WriteLine("Вы ввели не верное число, введите целое чисто от 1 до 7");
 if (num == 1) Console.WriteLine("Понедельник");
 if (num == 2) Console.WriteLine("Вторник");
 if (num == 3) Console.WriteLine("Среда");
