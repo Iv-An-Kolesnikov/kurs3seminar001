d2691bb5d8f14649d8666242a0aec56082732152
Iv-An-Kolesnikov <i_a_kolesnikov@list.ru>
2022-10-14 12:25:35 +0300

    task003 finich

--- task003/Program.cs
+++ task003/Program.cs
@@ -1,3 +1,27 @@
 ﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все целые числа в промежутке от -N до N
 // 4 => "-4, -3, -2, -1, 0, 1, 2, 3, 4"
 // 2 => "-2, -1, 0, 1, 2"
+Console.Clear();
+int input;
+//int output;
+//int raschet;
+int cycle1 = 0;
+int cycle2 = 1;
+int predel1;
+int predel2;
+Console.WriteLine("Введите число");
+input = Convert.ToInt32(Console.ReadLine());
+predel1 = input * 2;
+predel2 = 2;
+input = input * (-1);
+while (cycle2 < predel2)
+{    
+    while (cycle1 < predel1)
+    {
+        Console.Write(input + ", ");
+        input = input + 1;
+        cycle1++;
+    }
+    cycle2++;
+    Console.Write(input);
+}
