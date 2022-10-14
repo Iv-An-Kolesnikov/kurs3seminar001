2bc7b209e10722eea2f293c984b89045d22ea10d
Iv-An-Kolesnikov <i_a_kolesnikov@list.ru>
2022-10-14 13:13:57 +0300

    task004 finich

--- task004/Program.cs
+++ task004/Program.cs
@@ -2,3 +2,14 @@
 // 456 => 6
 // 782 => 2
 // 918 => 8
+Console.Clear();
+int input;
+int number = 0;
+Console.WriteLine("Введите трехзначное число");
+input = Convert.ToInt32(Console.ReadLine());
+if ((input > 99) & (input < 1000)) 
+{
+    number = input % 10;
+    Console.Write("Последняя цифра вверденного числа: " + number);
+}
+else Console.WriteLine("Введенное число не трехзначное, введите трехзначное число");
